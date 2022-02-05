3545ab28fdb20f63771b665931ffe48bbd3ac401
AlekseyVY <alekseyvy@gmail.com>
2022-02-05 14:57:01 +0300

    feat: header bottom part styles

--- src/components/header/Header.style.ts
+++ src/components/header/Header.style.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { black, white } from '../../styles/color';
+import { black, grWhite, white } from '../../styles/color';
 
 export const HeaderStyle = styled.header`
   width: 100%;
@@ -72,4 +72,84 @@ export const RightElementsContainerStyle = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+`;
+
+export const BottomBarContainerStyle = styled.div`
+  width: 100%;
+  height: 66px;
+  border-bottom: 1px solid ${grWhite};
+`;
+
+export const BottomInnerContainerStyle = styled.div`
+  max-width: 1110px;
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const LogoStyle = styled.h1`
+  font-family: Playfair Display SC,serif;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 32px;
+  letter-spacing: 0;
+  text-align: left;
+`;
+
+export const NavContainerStyle = styled.nav`
+  max-width: 585px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 16px;
+  letter-spacing: 0;
+  text-align: left;
+`;
+
+export const LinkStyle = styled.div`
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const ActionMenuContainerStyle = styled.div`
+  width: 172px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const ActionElementStyle = styled.div`
+  position: relative;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const CircleStyle = styled.div`
+  position: absolute;
+  top: -3px;
+  right: -3px;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${black};
+  font-size: 9px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 18px;
+  letter-spacing: 0;
+  text-align: left;
+  color: ${white};
 `;
