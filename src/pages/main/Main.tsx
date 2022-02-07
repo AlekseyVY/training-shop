b156fe1281026d017535e90196860a50174ac1b0
AlekseyVY <alekseyvy@gmail.com>
2022-02-07 17:18:39 +0300

    feat: Main page hero block

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,10 +1,11 @@
 import Header from '../../components/header/Header';
+import Hero from '../../components/hero/Hero';
 
 function Main() {
   return (
     <>
       <Header />
-      <h1>main</h1>
+      <Hero />
     </>
   );
 }
