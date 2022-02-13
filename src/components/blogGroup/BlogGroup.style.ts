76c042d6cc7f04edf04e211677e12f067be918b4
AlekseyVY <alekseyvy@gmail.com>
2022-02-13 17:02:08 +0300

    feat: blog group component styles

---
+++ src/components/blogGroup/BlogGroup.style.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const ContainerStyle = styled.section`
+  max-width: 1110px;
+  width: 100%;
+  margin: 96px auto;
+`;
+
+export const HeaderContainerStyle = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TitleStyle = styled.h3`
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 25px;
+  letter-spacing: 0.06em;
+  text-align: left;
+  text-transform: uppercase;
+`;
+
+export const TextStyle = styled.p`
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 13px;
+  letter-spacing: 0.06em;
+  text-align: right;
+  text-transform: uppercase;
+  opacity: 0.6;
+`;
+
+export const BlogsContainerStyle = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 48px;
+`;
