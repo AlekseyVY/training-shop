6ea1abab88a85ebe35be72ca820170b18dbed80e
AlekseyVY <alekseyvy@gmail.com>
2022-02-13 13:54:39 +0300

    feat: men products tmp dataset

---
+++ src/data/menMainProducts.ts
@@ -0,0 +1,76 @@
+import m1 from '../assets/jpg/m1.jpg';
+import m2 from '../assets/jpg/m2.jpg';
+import m3 from '../assets/jpg/m3.jpg';
+import m4 from '../assets/jpg/m4.jpg';
+import m5 from '../assets/jpg/m5.jpg';
+import m6 from '../assets/jpg/m6.jpg';
+import m7 from '../assets/jpg/m7.jpg';
+import m8 from '../assets/jpg/m8.jpg';
+import rate from '../assets/png/tmp_rating.png';
+
+export const MEN_PRODUCTS = [
+  {
+    id: '1',
+    img: m1,
+    title: 'Women\'s tracksuit Q109',
+    price: '30.00',
+    rating: rate,
+    discount: 0,
+  },
+  {
+    id: '2',
+    img: m2,
+    title: 'Women\'s tracksuit Q109',
+    price: '30.00',
+    rating: rate,
+    discount: 0,
+  },
+  {
+    id: '3',
+    img: m3,
+    title: 'Women\'s tracksuit Q109',
+    price: '30.00',
+    rating: rate,
+    discount: 0,
+  },
+  {
+    id: '4',
+    img: m4,
+    title: 'Women\'s tracksuit Q109',
+    price: '30.00',
+    rating: rate,
+    discount: 0,
+  },
+  {
+    id: '5',
+    img: m5,
+    title: 'Women\'s tracksuit Q109',
+    price: '30.00',
+    rating: rate,
+    discount: 0,
+  },
+  {
+    id: '6',
+    img: m6,
+    title: 'Women\'s tracksuit Q109',
+    price: '30.00',
+    rating: rate,
+    discount: 0,
+  },
+  {
+    id: '7',
+    img: m7,
+    title: 'Women\'s tracksuit Q109',
+    price: '30.00',
+    rating: rate,
+    discount: 0,
+  },
+  {
+    id: '8',
+    img: m8,
+    title: 'Women\'s tracksuit Q109',
+    price: '30.00',
+    rating: rate,
+    discount: 0,
+  },
+];
