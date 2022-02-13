c2f67bcea477195f2e1c25c471aad4b06724b792
AlekseyVY <alekseyvy@gmail.com>
2022-02-13 16:59:27 +0300

    feat: tmp data for middle promo component

---
+++ src/data/middlePromo.ts
@@ -0,0 +1,22 @@
+import p1 from '../assets/jpg/p1.jpg';
+import p2 from '../assets/jpg/p2.jpg';
+
+export const PROMO_DATA = [
+  {
+    width: 540,
+    height: 260,
+    background: p1,
+    banner: 'new season',
+    title: 'lookbook collection',
+    titleSize: 16,
+  },
+  {
+    width: 540,
+    height: 260,
+    background: p2,
+    banner: 'sale',
+    title: 'Get UP to',
+    promo: '50% off',
+    titleSize: 16,
+  },
+];
