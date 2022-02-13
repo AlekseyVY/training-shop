23365afbd016c83248f814d9774dee16122c520c
AlekseyVY <alekseyvy@gmail.com>
2022-02-13 16:59:06 +0300

    feat: tmp data for blog component

---
+++ src/data/blogData.ts
@@ -0,0 +1,24 @@
+import blog1 from '../assets/jpg/blog1.jpg';
+import blog2 from '../assets/jpg/blog2.jpg';
+import blog3 from '../assets/jpg/blog3.jpg';
+
+export const BLOG_DATA = [
+  {
+    id: '1',
+    img: blog1,
+    title: 'The Easiest Way to Break',
+    text: 'But I must explain to you how all this mistaken idea of denouncing pleas and praising pain was bor',
+  },
+  {
+    id: '1',
+    img: blog2,
+    title: 'Wedding Season',
+    text: 'But I must explain to you how all this mistaken idea of denouncing pleas and praising pain was bor',
+  },
+  {
+    id: '1',
+    img: blog3,
+    title: 'Recent Favorites On Repeat',
+    text: 'But I must explain to you how all this mistaken idea of denouncing pleas and praising pain was bor',
+  },
+];
