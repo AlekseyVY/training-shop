49758f1d28be785093708acd4aaf184f0f9f7f7f
AlekseyVY <alekseyvy@gmail.com>
2022-02-13 17:03:19 +0300

    feat: partially completed main page

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,8 +1,14 @@
+import { v4 as uuidv4 } from 'uuid';
 import Header from '../../components/header/Header';
 import Hero from '../../components/hero/Hero';
 import MainGroup from '../../components/mainGroup/MainGroup';
 import { WOMEN_PRODUCTS } from '../../data/womanMainProducts';
 import { MEN_PRODUCTS } from '../../data/menMainProducts';
+import CategoryCard from '../../components/categoryCard/CategoryCard';
+import { PROMO_DATA } from '../../data/middlePromo';
+import { PromoContainerStyle } from './Main.style';
+import SubscribePromo from '../../components/subscribePromo/SubscribePromo';
+import BlogGroup from '../../components/blogGroup/BlogGroup';
 
 function Main() {
   return (
@@ -11,6 +17,23 @@ function Main() {
       <Hero />
       <MainGroup title={'women\'s'} products={WOMEN_PRODUCTS} />
       <MainGroup title={'men\'s'} products={MEN_PRODUCTS} />
+      <PromoContainerStyle>
+        {PROMO_DATA.map((ele) => (
+          <div key={uuidv4()}>
+            <CategoryCard
+              width={ele.width}
+              height={ele.height}
+              background={ele.background}
+              title={ele.title}
+              banner={ele.banner}
+              promo={ele.promo}
+              titleSize={ele.titleSize}
+            />
+          </div>
+        ))}
+      </PromoContainerStyle>
+      <SubscribePromo />
+      <BlogGroup />
     </>
   );
 }
